94261c016679c77eef09266c4ded2e7fc79688f0
Joey Pettit <pettitbass@gmail.com>
2023-01-13 23:33:14 -0600

    added login page, socketProvider, and useLocalStorage hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import PlayerHand from "./Player/PlayerHand";
 import GameBoard from "./Gameboard/GameBoard";
-import axios from "axios";
 
 function App() {
-  const [gameState, setGameState] = useState({});
-
-  // this function will request a new game object from server and set it to state
-  function newGame(numOfPlayers) {
-    axios({
-      method: "GET",
-      url: `/api/game/new/`,
-    })
-      .then((response) => {
-        console.log(response.data);
-        setGameState(response.data);
-      })
-      .catch((error) => console.log(error));
-  }
-
-  function newRound() {
-    axios({
-      method: "GET",
-      url: `/api/game/newround/`,
-    })
-      .then((response) => {
-        console.log("new round successful");
-      })
-      .catch((error) => console.log(error));
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         💀💀 & 🌹🌹
-        <button onClick={() => newGame()}>New Game</button>
-        <button onClick={() => newRound()}>New Round</button>
+        <button>New Game</button>
+        <button>New Round</button>
       </header>
 
       <div className="player-hands">
         <h1>Player Hands:</h1>
-        <PlayerHand playerId={1} />
-        <PlayerHand playerId={2} />
-        <PlayerHand playerId={3} />
+        <PlayerHand playerNum={1} />
+        <PlayerHand playerNum={2} />
+        <PlayerHand playerNum={3} />
       </div>
 
-      <GameBoard gameState={gameState} />
+      <GameBoard />
     </div>
   );
 }
--- src/Player/PlayerHand.jsx
+++ src/Player/PlayerHand.jsx
@@ -1,18 +1,14 @@
-import PlayerCard from "./PlayerCard";
-import axios from "axios";
-import { useState, useEffect } from "react";
+// import PlayerCard from "./PlayerCard";
 
-function PlayerHand({ playerId }) {
-  const [playerState, setPlayerState] = useState({});
-
+function PlayerHand({ playerNum }) {
   return (
     <div>
-      <h3>{Object.keys(playerState).length !== 0 && playerState.name}</h3>
+      {/* <h3>{Object.keys(playerState).length !== 0 && playerState.name}</h3>
       <h4>Hand is:</h4>
       {Object.keys(playerState).length !== 0 &&
         playerState.cardsInHand.map((card) => {
           return <PlayerCard card={card} />;
-        })}
+        })} */}
 
       <button>Update</button>
     </div>
